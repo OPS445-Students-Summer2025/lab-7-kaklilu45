21986c8c6be9c6b3958f9ab600317d0eea545189
Kerubel Aklilu <kaklilu@myseneca.ca>
2025-07-17 17:59:16 -0400

    upload of lab7 scripts

--- lab7e.py
+++ lab7e.py
@@ -13,6 +13,17 @@ class Time:
         self.minute = minute
         self.second = second
 
+    def __str__(self):
+        """Return a string representation for the object using ':' as separator.
+        """
+        return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
+
+    def __repr__(self):
+        """Return a string representation for the object using '.' as separator. """
+        '''just instead of ':', you are required use the '.'  in the formatting string.'''
+
+        return f'{self.hour:02d}.{self.minute:02d}.{self.second:02d}'
+
     def format_time(self):
         """Return time object (t) as a formatted string"""
         return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
--- lab7f.py
+++ lab7f.py
@@ -13,6 +13,21 @@ class Time:
         self.minute = minute
         self.second = second
 
+    def __str__(self):
+        """Return a string representation for the object using ':' as separator.
+        """
+        return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
+
+    def __repr__(self):
+        """Return a string representation for the object using '.' as separator. """
+        '''just instead of ':', you are required use the '.'  in the formatting string.'''
+
+        return f'{self.hour:02d}.{self.minute:02d}.{self.second:02d}'
+
+    def __add__(self, t2):
+        """return the result by using sum_times() method""" 
+        return self.sum_times(t2)
+
     def format_time(self):
         """Return time object (t) as a formatted string"""
         return f'{self.hour:02d}:{self.minute:02d}:{self.second:02d}'
